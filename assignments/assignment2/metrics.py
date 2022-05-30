be7c056f5418c81d4c60728ff21c272f90ec2eaf
goluckywizard <73885997+goluckywizard@users.noreply.github.com>
2022-05-31 01:02:43 +0700

    Update metrics.py

--- assignments/assignment2/metrics.py
+++ assignments/assignment2/metrics.py
@@ -1,5 +1,32 @@
+def binary_classification_metrics(prediction, ground_truth):
+    '''
+    Computes metrics for binary classification
+
+    Arguments:
+    prediction, np array of bool (num_samples) - model predictions
+    ground_truth, np array of bool (num_samples) - true labels
+
+    Returns:
+    precision, recall, f1, accuracy - classification metrics
+    '''
+    true_positive = np.sum(np.logical_and(prediction, ground_truth))
+    false_positive = np.sum(np.greater(prediction, ground_truth))
+    false_negative = np.sum(np.less(prediction, ground_truth))
+    precision = true_positive / (true_positive + false_positive)
+    recall = true_positive / (true_positive + false_negative)
+    accuracy = np.sum(prediction == ground_truth) / prediction.size
+    f1 = precision*recall / (precision + recall)
+
+    # TODO: implement metrics!
+    # Some helpful links:
+    # https://en.wikipedia.org/wiki/Precision_and_recall
+    # https://en.wikipedia.org/wiki/F1_score
+    
+    return precision, recall, f1, accuracy
+
+
 def multiclass_accuracy(prediction, ground_truth):
-    """
+    '''
     Computes metrics for multiclass classification
 
     Arguments:
@@ -8,9 +35,6 @@ def multiclass_accuracy(prediction, ground_truth):
 
     Returns:
     accuracy - ratio of accurate predictions to total samples
-    """
-
+    '''
     # TODO: Implement computing accuracy
-    raise Exception("Not implemented!")
-
-    return 0
+    return np.sum(prediction == ground_truth)/prediction.size
