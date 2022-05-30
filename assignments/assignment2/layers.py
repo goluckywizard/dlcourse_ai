e07e613d819d0fa6a24cb90eea393bfa732a672d
goluckywizard <73885997+goluckywizard@users.noreply.github.com>
2022-05-31 03:14:39 +0700

    Update layers.py

--- assignments/assignment2/layers.py
+++ assignments/assignment2/layers.py
@@ -1,85 +1,21 @@
 import numpy as np
 
-
-def l2_regularization(W, reg_strength):
-    """
-    Computes L2 regularization loss on weights and its gradient
-
-    Arguments:
-      W, np array - weights
-      reg_strength - float value
-
-    Returns:
-      loss, single value - l2 regularization loss
-      gradient, np.array same shape as W - gradient of weight by l2 loss
-    """
-    # TODO: Copy from the previous assignment
-    #raise Exception("Not implemented!")
-    loss = reg_strength*np.sum(W**2)
-    grad = 2*reg_strength*W
-    #grad = np.dot(W,reg_strength)
-    return loss, grad
-
 def softmax(predictions):
     '''
     Computes probabilities from scores
-
     Arguments:
       predictions, np array, shape is either (N) or (batch_size, N) -
         classifier output
-
     Returns:
       probs, np array of the same shape as predictions - 
         probability for every class, 0..1
     '''
     # TODO implement softmax
+    # Your final implementation shouldn't have any loops
+    preds = (predictions.T - np.max(predictions, axis=1)).T
+    exps = np.exp(preds)
     
-    if len(predictions.shape) == 1:
-        probs = predictions.copy()
-        probs -= np.max(probs)
-        probs = np.exp(probs) / np.sum(np.exp(probs))
-        return probs
-    else:
-        probs = predictions.copy()
-        probs -= np.max(probs, axis = 1).reshape(-1,1)
-        probs = np.exp(probs)/np.sum(np.exp(probs),axis = 1).reshape(-1,1)
-        return probs
-    
-# def cross_entropy_loss(probs, target_index):
-#     '''
-#     Computes cross-entropy loss
-
-#     Arguments:
-#       probs, np array, shape is either (N) or (batch_size, N) -
-#         probabilities for every class
-#       target_index: np array of int, shape is (1) or (batch_size) -
-#         index of the true class for given sample(s)
-
-#     Returns:
-#       loss: single value
-#     '''
-#     # TODO implement cross-entropy
-    
-#     if len(probs.shape) == 1:
-#         return -np.log(probs[target_index])
-#     else:
-#         #batch
-#         return -np.sum(np.log(probs[np.arange(len(probs)), target_index]))
-
-    
-# def softmax(predictions):
-#     '''
-#     Computes probabilities from scores
-#     Arguments:
-#       predictions, np array, shape is either (N) or (batch_size, N) -
-#         classifier output
-#     Returns:
-#       probs, np array of the same shape as predictions -
-#         probability for every class, 0..1
-#     '''
-#     c = np.max(predictions, keepdims=True, axis=1)
-#     soft_max = np.exp(predictions - c)/np.sum(np.exp(predictions - c), keepdims=True, axis=1)
-#     return soft_max
+    return (exps.T / exps.sum(axis=1)).T
 
 
 def cross_entropy_loss(probs, target_index):
@@ -93,50 +29,48 @@ def cross_entropy_loss(probs, target_index):
     Returns:
       loss: single value
     '''
-    px = np.zeros_like(probs)
-    for row in range(px.shape[0]):
-        px[row,target_index[row]] = 1.
-    qx = probs
+    # TODO implement cross-entropy
+    # Your final implementation shouldn't have any loops
+    return - np.log(probs[np.arange(len(probs)), target_index]).mean()
 
-    ces = np.zeros_like(target_index, dtype=np.float)
-    for ix in range(0, target_index.shape[0]):
-        ces[ix]= -1. * np.sum(px[ix] * np.log(qx[ix]))
-    return  ces.reshape((target_index.shape[0],1))  
+def l2_regularization(W, reg_strength):
+    """
+    Computes L2 regularization loss on weights and its gradient
+    Arguments:
+      W, np array - weights
+      reg_strength - float value
+    Returns:
+      loss, single value - l2 regularization loss
+      gradient, np.array same shape as W - gradient of weight by l2 loss
+    """
+    # TODO: Copy from the previous assignment
+    loss = reg_strength * np.power(W, 2).sum()
+    grad = reg_strength * 2 * W
+    
+    return loss, grad
+
 
 def softmax_with_cross_entropy(preds, target_index):
     """
     Computes softmax and cross-entropy loss for model predictions,
     including the gradient
-
     Arguments:
       predictions, np array, shape is either (N) or (batch_size, N) -
         classifier output
       target_index: np array of int, shape is (1) or (batch_size) -
         index of the true class for given sample(s)
-
     Returns:
       loss, single value - cross-entropy loss
       dprediction, np array same shape as predictions - gradient of predictions by loss value
     """
     # TODO: Copy from the previous assignment
-#     probs = softmax(preds)
-#     loss = cross_entropy_loss(probs, target_index)   
-#     dprediction = probs.copy()
+    probs = softmax(preds)
+    loss = cross_entropy_loss(probs, target_index).mean()
+    dprediction = np.zeros_like(preds)
+    dprediction[np.arange(preds.shape[0]), target_index] = 1
+    dprediction = - (dprediction - softmax(preds)) / dprediction.shape[0]
     
-#     if len(preds.shape) == 1:
-#         dprediction[target_index] = dprediction[target_index] - 1
-#     else:
-#         dprediction[np.arange(len(dprediction)), target_index] -=1
- 
-#     return np.mean(loss), dprediction
-    sft_max = softmax(preds)
-    real1 = np.zeros_like(preds)
-    for row in range(real1.shape[0]):
-        real1[row,target_index[row]] = 1.
-    der = sft_max - real1
-    loss = cross_entropy_loss(sft_max, target_index)
-    return np.mean(loss), der/loss.shape[0]
-
+    return loss, dprediction
 
 
 class Param:
@@ -152,38 +86,37 @@ class Param:
 
 class ReLULayer:
     def __init__(self):
-        pass
+        self.is_positive = True
 
     def forward(self, X):
         # TODO: Implement forward pass
         # Hint: you'll need to save some information about X
         # to use it later in the backward pass
-        out = np.maximum(0,X)
-        self.cashe = X
-        return out
+        output = np.maximum(0, X)
+        self.is_positive = output.astype(bool)
+        
+        return output
+        
 
     def backward(self, d_out):
         """
         Backward pass
-
         Arguments:
         d_out, np array (batch_size, num_features) - gradient
            of loss function with respect to output
-
         Returns:
         d_result: np array (batch_size, num_features) - gradient
           with respect to input
         """
         # TODO: Implement backward pass
-        # Your final implementation shouldn't have any loops       
-        d_result = d_out.copy()
-        d_result[self.cashe < 0] = 0     
+        # Your final implementation shouldn't have any loops
+        d_result = self.is_positive.astype(int) * d_out
+        
         return d_result
 
     def params(self):
         # ReLU Doesn't have any parameters
         return {}
-
 
 class FullyConnectedLayer:
     def __init__(self, n_input, n_output):
@@ -195,19 +128,17 @@ class FullyConnectedLayer:
         # TODO: Implement forward pass
         # Your final implementation shouldn't have any loops
         self.X = X
-        output = np.dot(self.X, self.W.value) + self.B.value
-        return output
+        
+        return self.X @ self.W.value + self.B.value
 
     def backward(self, d_out):
         """
         Backward pass
         Computes gradient with respect to input and
         accumulates gradients within self.W and self.B
-
         Arguments:
         d_out, np array (batch_size, n_output) - gradient
            of loss function with respect to output
-
         Returns:
         d_result: np array (batch_size, n_input) - gradient
           with respect to input
@@ -218,10 +149,11 @@ class FullyConnectedLayer:
         # Add gradients of W and B to their `grad` attribute
 
         # It should be pretty similar to linear classifier from
-        # the previous assignment        
-        self.W.grad = np.dot(self.X.T, d_out)
-        self.B.grad = np.dot(np.ones([1, d_out.shape[0]]), d_out)
-        d_input = np.dot(d_out, self.W.value.T)        
+        # the previous assignment
+        self.W.grad += self.X.T @ d_out
+        self.B.grad += 2 * np.mean(d_out, axis=0)
+        d_input = d_out @ self.W.value.T
+        
         return d_input
 
     def params(self):
